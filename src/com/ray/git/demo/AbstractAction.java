782ad2f8ee198b56f91684f982bc59b6e2bbf506
bizz <bizarry.wong@gmail.com>
2014-08-29 14:54:35 +0800

    hello world.

---
+++ src/com/ray/git/demo/AbstractAction.java
@@ -0,0 +1,13 @@
+package com.ray.git.demo;
+
+public class AbstractAction {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
