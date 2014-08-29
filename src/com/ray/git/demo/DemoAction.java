d9fa282b195d430fb875d22d3c49b266429b0214
bizz <bizarry.wong@gmail.com>
2014-08-29 14:59:29 +0800

    Ray commits demo action for demo process.

---
+++ src/com/ray/git/demo/DemoAction.java
@@ -0,0 +1,13 @@
+package com.ray.git.demo;
+
+public class DemoAction extends AbstractAction {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
